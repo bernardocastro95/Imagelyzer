a9cb72941621b0a2be422d0165492d8917fa7125
bernardocastro95 <bernardoc544@gmail.com>
2026-08-05 21:02:54 -0400

    finished layout and added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
